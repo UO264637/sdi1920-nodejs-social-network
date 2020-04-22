f2ac6a7a67df8f6fcb3f394be3226f8d822a105f
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-04-22 12:57:00 +0200

    Controllers implementation

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-// Módulos
+/*****************************************************************************\
+									MODULOS
+\*****************************************************************************/
+
+// Express
 let express = require("express");
 let app = express();
+app.use(express.static("public"));						// Sets the static folder
+
+// Swig
+let swig = require("swig");
+
+// Body Parser
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Variables
 app.set("port", 8081);
 
-// GET /home
-app.get("/home", function(req, res) {
-	res.send("home");
-});
+/*****************************************************************************\
+ 								CONTROLADORES
+\*****************************************************************************/
 
-// Lanzamiento del servidor
+require("./routes/rusers")(app, swig);						// Users controller
+
+/*****************************************************************************\
+ 						LANZAMIENTO DEL SERVIDOR
+\*****************************************************************************/
 app.listen(app.get("port"), function() {
 	console.log("Server deployed at http://localhost:" + app.get("port") + "/home");
 });
---
+++ routes/rusers.js
@@ -0,0 +1,14 @@
+module.exports = function(app, swig) {
+	app.get("/users", function(req, res) {
+
+		const fs = require("fs");
+		let defaultdb = JSON.parse(fs.readFileSync("config/defaultdb.json"));
+		console.log(defaultdb);
+
+		let answer = swig.renderFile("views/users.html", {
+			users: defaultdb.users
+		});
+
+		res.send(answer);
+	});
+};
