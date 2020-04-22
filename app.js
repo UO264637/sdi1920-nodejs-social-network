d0d257971fb6c1d949b614dced324711bfb2ec00
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-04-22 21:26:41 +0200

    Implementation of database

--- app.js
+++ app.js
@@ -10,19 +10,26 @@ app.use(express.static("public"));						// Sets the static folder
 // Swig
 let swig = require("swig");
 
+// MongoDB
+let mongo = require('mongodb');
+
 // Body Parser
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Variables
+/*****************************************************************************\
+ 									VARIABLES
+\*****************************************************************************/
+
 app.set("port", 8081);
+app.set("db", "mongodb://admin:viade_es4c@mysocialnetwork-shard-00-00-mtis7.mongodb.net:27017,mysocialnetwork-shard-00-01-mtis7.mongodb.net:27017,mysocialnetwork-shard-00-02-mtis7.mongodb.net:27017/test?ssl=true&replicaSet=MySocialNetwork-shard-0&authSource=admin&retryWrites=true&w=majority");
 
 /*****************************************************************************\
  								CONTROLADORES
 \*****************************************************************************/
 
-require("./routes/rusers")(app, swig);						// Users controller
+require("./routes/rusers")(app, swig, mongo);									// Users controller
 
 /*****************************************************************************\
  						LANZAMIENTO DEL SERVIDOR
