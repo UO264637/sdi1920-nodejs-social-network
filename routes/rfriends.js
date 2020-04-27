b6c70f14a86391962fd47814f721e8c5d44c3482
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-04-27 13:02:12 +0200

    Friend listing

--- routes/rfriends.js
+++ routes/rfriends.js
@@ -40,6 +40,33 @@ module.exports = function(app, dbManager) {
 	});
 
 	/**
+	 * List the user friends
+	 */
+	app.get("/friend/list", (req, res) => {
+		// Load the user
+		dbManager.get("users", {email: req.session.user}, (result) => {
+			if (result == null)
+				console.error("Unable to retrieve the current user");
+			else {
+				// Loads the friends
+				let query = {_id: {$in: result[0].friends}};
+				let pg = req.query.pg ? parseInt(req.query.pg) : 1;
+				dbManager.getPg("users", query, pg, (results, count) => {
+					// Prepares the pagination
+					let pages = [];
+					for (let i = pg-2; i<=pg+2; i++) pages.push(i);
+					// Sends the page
+					res.send(app.generateView("views/friend/list.html", req.session, {
+						friends: results,
+						pages: pages.filter((i) => {return (i > 0 && i <= Math.ceil(count/5))}),
+						current: pg,
+					}));
+				});
+			}
+		});
+	});
+
+	/**
 	 * List the incoming friend requests of the user
 	 */
 	app.get("/friend/requests", (req, res) => {
