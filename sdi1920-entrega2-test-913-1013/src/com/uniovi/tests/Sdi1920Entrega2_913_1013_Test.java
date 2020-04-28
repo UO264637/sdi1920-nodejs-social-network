9457f8ffe0df0fe6b6debfddceb25985c67babd7
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-04-28 13:21:14 +0200

    User list tests and tests documentation
    Following #32

--- sdi1920-entrega2-test-913-1013/src/com/uniovi/tests/Sdi1920Entrega2_913_1013_Test.java
+++ sdi1920-entrega2-test-913-1013/src/com/uniovi/tests/Sdi1920Entrega2_913_1013_Test.java
@@ -4,13 +4,9 @@ import org.junit.*;
 import org.junit.runners.MethodSorters;
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
-
 //Paquetes Selenium 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.*;
-//Paquetes Utilidades de Testing Propias
-import com.uniovi.tests.util.SeleniumUtils;
 //Paquetes con los Page Object
 import com.uniovi.tests.pageobjects.*;
 
@@ -66,7 +62,13 @@ public class Sdi1920Entrega2_913_1013_Test {
 	}
 
 	/********************************************************************************\
-	  										SIGN UP TESTS
+	
+										WEB APPLICATION
+		
+	\********************************************************************************/
+	
+	/********************************************************************************\
+	  									W1 - REGISTER
 	\********************************************************************************/
 
 	/**
@@ -74,9 +76,14 @@ public class Sdi1920Entrega2_913_1013_Test {
 	 */
 	@Test
 	public void PR01() {
-		PO_HomeView.goToRegister(driver);
+		// We check always the homepage (login) is loaded
+		PO_LoginView.checkElement(driver, "id", "email");
+		// Then we navigate to the register
+		PO_LoginView.goToRegister(driver);
+		// Fill the form with valid data
 		PO_RegisterView.fillForm(driver, "Lopen", "Bridge 4", "lopen@bridge4.com", "12345678", "12345678");
-		PO_View.checkElement(driver, "id", "tableUsers");
+		// And check we are redirected to the users list
+		PO_UsersView.checkElement(driver, "id", "tableUsers");
 	}
 
 	/**
@@ -86,14 +93,16 @@ public class Sdi1920Entrega2_913_1013_Test {
 	 */
 	@Test
 	public void PR02() {
-		PO_HomeView.goToRegister(driver);
-		// Empty email
+		PO_LoginView.checkElement(driver, "id", "email");
+		PO_LoginView.goToRegister(driver);
+		// We fill the form with invalid data (empty email)
 		PO_RegisterView.fillForm(driver, "Lopen", "Bridge 4", "", "12345678", "12345678");
+		// And we check we still are in the register page
 		PO_RegisterView.checkElement(driver, "id", "iName");
-		// Empty name
+		// Same with empty name
 		PO_RegisterView.fillForm(driver, "", "Bridge 4", "lopen@bridge4.com", "12345678", "12345678");
 		PO_RegisterView.checkElement(driver, "id", "iName");
-		// Empty surname
+		// And empty surname
 		PO_RegisterView.fillForm(driver, "", "Bridge 4", "lopen@bridge4.com", "12345678", "12345678");
 		PO_RegisterView.checkElement(driver, "id", "iName");
 	}
@@ -103,12 +112,16 @@ public class Sdi1920Entrega2_913_1013_Test {
 	 */
 	@Test
 	public void PR02_1() {
-		PO_HomeView.goToRegister(driver);
-		// Too short name
+		PO_LoginView.checkElement(driver, "id", "email");
+		PO_LoginView.goToRegister(driver);
+		// We fill the form with invalid data (too short name)
 		PO_RegisterView.fillForm(driver, "a", "Bridge 4", "lopen@bridge4.com", "12345678", "12345678");
+		// And we check we still are in the register page and got an alert
+		PO_RegisterView.checkElement(driver, "id", "iName");
 		PO_RegisterView.checkElement(driver, "class", "alert");
-		// Too short surname
+		// Same with too short surname
 		PO_RegisterView.fillForm(driver, "Lopen", "a", "lopen@bridge4.com", "12345678", "12345678");
+		PO_RegisterView.checkElement(driver, "id", "iName");
 		PO_RegisterView.checkElement(driver, "class", "alert");		
 	}
 	
@@ -117,9 +130,11 @@ public class Sdi1920Entrega2_913_1013_Test {
 	 */
 	@Test
 	public void PR02_2() {
-		PO_HomeView.goToRegister(driver);
-		// Too short name
+		PO_LoginView.checkElement(driver, "id", "email");
+		PO_LoginView.goToRegister(driver);
+		// Same as previous test with too short password
 		PO_RegisterView.fillForm(driver, "Lopen", "Bridge 4", "lopen@bridge4.com", "12", "12");
+		PO_RegisterView.checkElement(driver, "id", "iName");
 		PO_RegisterView.checkElement(driver, "class", "alert");		
 	}
 
@@ -128,9 +143,11 @@ public class Sdi1920Entrega2_913_1013_Test {
 	 */
 	@Test
 	public void PR03() {
-		PO_HomeView.goToRegister(driver);
-		// Too short name
+		PO_LoginView.checkElement(driver, "id", "email");
+		PO_LoginView.goToRegister(driver);
+		// Same as previous test with not matching passwords
 		PO_RegisterView.fillForm(driver, "Lopen", "Bridge 4", "lopen@bridge4.com", "12345678", "abcdefgh");
+		PO_RegisterView.checkElement(driver, "id", "iName");
 		PO_RegisterView.checkElement(driver, "class", "alert");			
 	}
 	
@@ -139,161 +156,394 @@ public class Sdi1920Entrega2_913_1013_Test {
 	 */
 	@Test
 	public void PR04() {
-		PO_HomeView.goToRegister(driver);
-		// Too short name
+		PO_LoginView.checkElement(driver, "id", "email");
+		PO_LoginView.goToRegister(driver);
+		// Same as previous test with already used email
 		PO_RegisterView.fillForm(driver, "Dalinar", "Kholin", "dalinar@kholin.com", "12345678", "12345678");
+		PO_RegisterView.checkElement(driver, "id", "iName");
 		PO_RegisterView.checkElement(driver, "class", "alert");		
 	}
 	
-	//PR05. Sin hacer /
+	/********************************************************************************\
+										W2 - LOGIN
+	\********************************************************************************/
+	
+	/**
+	 * PR05 - Login with valid data, standard user
+	 */
 	@Test
 	public void PR05() {
 		assertTrue("PR05 sin hacer", false);			
 	}
 	
-	//PR06. Sin hacer /
+	/**
+	 * PR06 - Login with invalid data, standard user, empty email and password
+	 */
 	@Test
 	public void PR06() {
 		assertTrue("PR06 sin hacer", false);			
 	}
 	
-	//PR07. Sin hacer /
+	/**
+	 * PR07 - Login with invalid data, standard user, correct email with incorrect password
+	 */
 	@Test
 	public void PR07() {
 		assertTrue("PR07 sin hacer", false);			
 	}	
 	
-	//PR08. Sin hacer /
+	/**
+	 * PR08 - Login with invalid data, standard user, incorrect email with not empty password
+	 */
 	@Test
 	public void PR08() {
 		assertTrue("PR08 sin hacer", false);			
 	}	
 	
-	//PR09. Sin hacer /
+	/********************************************************************************\
+										W3 - LOGOUT
+	\********************************************************************************/
+	
+	/**
+	 * PR09. Logout. As logged user use the logout function and test it works properly
+	 */
 	@Test
 	public void PR09() {
 		assertTrue("PR09 sin hacer", false);			
 	}	
-	//PR10. Sin hacer /
+	
+	/**
+	 * PR10. Logout. As anonymous user check the logout button is not visible.
+	 */
 	@Test
 	public void PR10() {
 		assertTrue("PR10 sin hacer", false);			
 	}	
 	
-	//PR11. Sin hacer /
+	/********************************************************************************\
+										W4 - USERS LIST
+	\********************************************************************************/
+	
+	/**
+	 *  PR11. List Users. Show Users and check it list all the users it should list.
+	 */
 	@Test
 	public void PR11() {
-		assertTrue("PR11 sin hacer", false);			
+		PO_LoginView.checkElement(driver, "id", "email");
+		// We log in and check we are in the Users page
+		PO_LoginView.logAs(driver, "dalinar@kholin.com", "123");	
+		PO_UsersView.checkElement(driver, "id", "tableUsers");
+		// We check the number of users in the first page (there should be five)
+		PO_UsersView.checkUsers(driver, "Adolin", "Renarin", "Kaladin", "Shallan", "Jasnah");
+		// We navigate to the second page
+		PO_UsersView.changePage(driver, 2);
 	}	
 	
-	//PR12. Sin hacer /
+	/********************************************************************************\
+										W5 - USERS SEARCH
+	\********************************************************************************/
+	
+	/**
+	 * PR12. Make a search with field empty and assert it shows all the users.
+	 */
 	@Test
 	public void PR12() {
 		assertTrue("PR12 sin hacer", false);			
 	}	
 	
-	//PR13. Sin hacer /
+	/**
+	 * PR13. Make a search with a non existant text and assert it shows an empty list.
+	 */
 	@Test
 	public void PR13() {
 		assertTrue("PR13 sin hacer", false);			
 	}	
 	
-	//PR14. Sin hacer /
+	/**
+	 *  PR14. Make a search with a valid text and assert it shows the correct users (more than 5).
+	 */
 	@Test
 	public void PR14() {
 		assertTrue("PR14 sin hacer", false);			
 	}	
 	
-	//PR15. Sin hacer /
+	
+	/********************************************************************************\
+									W6 - SEND FRIEND REQUEST
+	\********************************************************************************/
+	
+	
+	/**
+	 * PR15. Send a valid friend requests and assert it shows up.
+	 */
 	@Test
 	public void PR15() {
 		assertTrue("PR15 sin hacer", false);			
 	}	
 	
-	//PR16. Sin hacer /
+	/**
+	 * PR16. Try to send a request to an already requested user and assert to you can't.
+	 */
 	@Test
 	public void PR16() {
 		assertTrue("PR16 sin hacer", false);			
 	}	
 	
-	//PR017. Sin hacer /
+	/**
+	 *  PR16_1. Try to send a request to an already friend user and assert you can't.
+	 */
+	@Test
+	public void PR16_1() {
+		assertTrue("PR16_1 sin hacer", false);			
+	}
+	
+	/**
+	 *  PR16_1. Try to send a friend request to yourself (through URL) and assert you can't
+	 */
+	@Test
+	public void PR16_2() {
+		assertTrue("PR16_2 sin hacer", false);			
+	}
+	
+	/**
+	 *  PR16_3. Try to send a friend request to someone that requested you, check it redirects you to the requests page
+	 */
+	@Test
+	public void PR16_3() {
+		assertTrue("PR16_3 sin hacer", false);			
+	}
+	
+	/********************************************************************************\
+										W7 - REQUESTS LIST
+	\********************************************************************************/
+	
+	/**
+	 * PR17. List Requests. List all the received requests.
+	 */
 	@Test
 	public void PR17() {
 		assertTrue("PR17 sin hacer", false);			
 	}	
 	
-	//PR18. Sin hacer /
+	/**
+	 * PR17_1. List all the received requests with a user with no requests.
+	 */
+	@Test
+	public void PR17_1() {
+		assertTrue("PR17_1 sin hacer", false);			
+	}
+	
+	/********************************************************************************\
+										W8 - ACCEPT REQUEST
+	\********************************************************************************/
+	
+	/**
+	 * PR18. Accept a request amd check the friendship is established.
+	 */
 	@Test
 	public void PR18() {
 		assertTrue("PR18 sin hacer", false);			
 	}	
 	
-	//PR19. Sin hacer /
+	/**
+	 * PR18_1. Try to accept a non-existant friend request (through URL) and assert you can't.
+	 */
+	@Test
+	public void PR18_1() {
+		assertTrue("PR18_1 sin hacer", false);			
+	}
+	
+	/********************************************************************************\
+										W9 - FRIENDS LIST
+	\********************************************************************************/
+	
+	/**
+	 * PR19. List Friends. Show the list of friends of the user and assert it shows what it must.
+	 */
 	@Test
 	public void PR19() {
 		assertTrue("PR19 sin hacer", false);			
 	}	
 	
-	//P20. Sin hacer /
+	/**
+	 * PR19_1. List Friends. Show the empty list of a user without friends.
+	 */
+	@Test
+	public void PR19_1() {
+		assertTrue("PR19_1 sin hacer", false);			
+	}
+	
+	/********************************************************************************\
+										W10 - SECURITY
+	\********************************************************************************/
+	
+	/**
+	 * PR20. Try to access the list of users as anonymous, check you can't.
+	 */
 	@Test
 	public void PR20() {
 		assertTrue("PR20 sin hacer", false);			
 	}	
 	
-	//PR21. Sin hacer /
+	/**
+	 * PR21. Try to access the list of requests as anonymous, check you can't
+	 */
 	@Test
 	public void PR21() {
 		assertTrue("PR21 sin hacer", false);			
 	}	
 	
-	//PR22. Sin hacer /
+	/**
+	 * PR22. Try to access the list of friends of other user.
+	 * 
+	 * With our design you can't access the list of friends of other users
+	 * 	as we take the list of friends from the current authenticated user
+	 * 
+	 * We replaced that test for a check from an anonymous user
+	 */
 	@Test
 	public void PR22() {
 		assertTrue("PR22 sin hacer", false);			
 	}	
 	
-	//PR23. Sin hacer /
+	/**
+	 * PR22_1. Try to access the list of friends as anonymous, check you can't
+	 */
+	@Test
+	public void PR22_1() {
+		assertTrue("PR22_1 sin hacer", false);			
+	}
+	
+	/**
+	 * PR22_2. Try to access the login form as logged user, check you can't
+	 */
+	@Test
+	public void PR22_2() {
+		assertTrue("PR22_2 sin hacer", false);			
+	}
+	
+	/**
+	 * PR22_3. Try to access the register form as logged user, check you can't
+	 */
+	@Test
+	public void PR22_3() {
+		assertTrue("PR22_3 sin hacer", false);			
+	}
+	
+
+	/********************************************************************************\
+	
+										CLIENT APPLICATION
+		
+	\********************************************************************************/
+	
+	/********************************************************************************\
+											C1 - LOGIN
+	\********************************************************************************/
+	
+	/**
+	 * PR23. Login with valid data
+	 */
 	@Test
 	public void PR23() {
 		assertTrue("PR23 sin hacer", false);			
 	}	
 	
-	//PR24. Sin hacer /
+	/**
+	 * PR24. Login with invalid data (non existant user)
+	 */
 	@Test
 	public void PR24() {
 		assertTrue("PR24 sin hacer", false);			
 	}	
-	//PR25. Sin hacer /
+	
+	/********************************************************************************\
+										C2 - FRIEND LIST
+	\********************************************************************************/	
+	
+	/**
+	 * PR25. Access the list of friends of an user with at least three friends.
+	 */
 	@Test
 	public void PR25() {
 		assertTrue("PR25 sin hacer", false);			
 	}	
 	
-	//PR26. Sin hacer /
+	/**
+	 * PR26. Access the list of friends of an user, filter to find one exact friend. The searched name must match.
+	 */
 	@Test
 	public void PR26() {
 		assertTrue("PR26 sin hacer", false);			
 	}	
 	
-	//PR27. Sin hacer /
+	
+	/********************************************************************************\
+										C3 - SHOW MESSAGES
+	\********************************************************************************/
+
+	/**
+	 * PR27. Access the list of messages of a friend (chat), it must contain aat least three messages.
+	 */
 	@Test
 	public void PR27() {
 		assertTrue("PR27 sin hacer", false);			
 	}	
 	
-	//PR029. Sin hacer /
+	
+	/********************************************************************************\
+										C4 - CREATE MESSAGES
+	\********************************************************************************/
+	
+	/**
+	 * PR28. Access the list of messages of a friend (chat) and create a new message.
+	 * 	Validate that it appears in the list of messages.
+	 */
+	@Test
+	public void PR28() {
+		assertTrue("PR28 sin hacer", false);			
+	}	
+	
+	
+	/********************************************************************************\
+										C5 - MARK AS READ AUTO
+	\********************************************************************************/
+
+	/**
+	 * PR29. Identify in the app and send a message to a friend, validate that the sent message appears in the chat. 
+	 * 	Identify next with the friend and validate that they have the message unread. 
+	 * 	Enter the chat and check the message is marked as read.
+	 */
 	@Test
 	public void PR29() {
 		assertTrue("PR29 sin hacer", false);			
 	}
-
-	//PR030. Sin hacer /
+	
+	
+	/********************************************************************************\
+										C6 - SHOW UNREAD MESSAGES
+	\********************************************************************************/
+	
+	/**
+	 * PR30. Identify in the app and send three messages to a friend, validate that the messages appears in the chat. 
+	 * 	Identify next with the friend and validate that the number of unread messages appears in the list of friends.
+	 */
 	@Test
 	public void PR30() {
 		assertTrue("PR30 sin hacer", false);			
 	}
 	
-	//PR031. Sin hacer /
+	
+	/********************************************************************************\
+										C7 - ORDER BY RECENT
+	\********************************************************************************/
+	
+	/**
+	 * PR31. Identify as user A with at least 3 friends. Go to the chat of the last friend and send him a message
+	 * 	Ggo back to the list of friends and check that user appears now the first in the list. 
+	 * 	Identify with the user B and send a message to user B. 
+	 * 	Go back to identift as user A and see that the user that send them the message appears the first in the list.
+	 */
 	@Test
 	public void PR31() {
 		assertTrue("PR31 sin hacer", false);			
--- sdi1920-entrega2-test-913-1013/src/com/uniovi/tests/pageobjects/PO_NavView.java
+++ sdi1920-entrega2-test-913-1013/src/com/uniovi/tests/pageobjects/PO_NavView.java
@@ -30,5 +30,22 @@ public class PO_NavView  extends PO_View{
 		//Tiene que haber un sólo elemento.
 		assertTrue(elementos.size()==1);	
 	}
+	
+	/**
+	 * Navigates to the Register page (only visible when not logged in)
+	 * @param driver
+	 */
+	static public void goToRegister(WebDriver driver) {
+		// We load the elements leading to the register
+		List<WebElement> elements = SeleniumUtils.EsperaCargaPagina(driver, "@href", "signup", getTimeout());
+		// There should be two elements
+		assertTrue(elements.size()==2);
+		// We click it
+		elements.get(0).click();
+		// And we wait for the register form to show up
+		elements = SeleniumUtils.EsperaCargaPagina(driver, "id", "iName", getTimeout());
+		// There should be only one element, the name input field
+		assertTrue(elements.size()==1);	
+	}
 
 }
